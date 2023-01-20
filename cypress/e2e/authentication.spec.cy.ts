3936dd00cdf813f3dbc0020278fb67f2335d777e
Jorge Andres Padilla <jorgeandres.padillasalgado@gmail.com>
2023-01-20 18:52:43 -0500

    Update E2E test cases

--- cypress/e2e/authentication.spec.cy.ts
+++ cypress/e2e/authentication.spec.cy.ts
@@ -1,13 +1,27 @@
 import { AppConfig } from '../../src/app/config/app.config';
 
 describe('Authentication', () => {
-  it('should login', () => {
+  it('should login as executive', () => {
     cy.visit(AppConfig.routes.authentication.login.fullPath);
-    cy.fixture('user').then((user) => {
+    cy.fixture('user').then((users) => {
+      const user = users.executive;
       cy.formControl('username').type(user.username);
       cy.formControl('password').type(user.password);
       cy.dataCy('login-button').click();
-      cy.url().should('include', AppConfig.routes.home.fullPath);
+      cy.url().should('include', AppConfig.routes.home);
+      cy.dataCy('add-application-button').should('not.exist');
+    });
+  });
+
+  it('should login as employee', () => {
+    cy.visit(AppConfig.routes.authentication.login.fullPath);
+    cy.fixture('user').then((users) => {
+      const user = users.employee;
+      cy.formControl('username').type(user.username);
+      cy.formControl('password').type(user.password);
+      cy.dataCy('login-button').click();
+      cy.url().should('include', AppConfig.routes.home);
+      cy.dataCy('add-application-button').should('exist');
     });
   });
 });
