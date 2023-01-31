f848f08b28aa6fe14b4c19c76ca33189623bd178
Jose Escudero <joseignacioescudero@gmail.com>
2023-01-30 21:58:47 -0500

    formulario

--- src/app/features/applications/pages/add-application/add-application.component.ts
+++ src/app/features/applications/pages/add-application/add-application.component.ts
@@ -14,35 +14,38 @@ import { AddApplication } from '../../services/types';
 export class AddApplicationComponent {
   addApplicationForm = new FormGroup({
     description: new FormControl('', [Validators.required]),
-    desde : new FormControl('', [Validators.required]), // texto
-    hasta : new FormControl('', [Validators.required]),
-    tipoTransporte : new FormControl('', [Validators.required]), //dropdown
-    perteneciente : new FormControl('', [Validators.required]),
-    fechaEmbarque: new FormControl('', [Validators.required]),
-    fechaLlegada: new FormControl('', [Validators.required]),
-    embarcadoPor : new FormControl('', [Validators.required]),
-    notaDePedido : new FormControl('', [Validators.required]), //dropdown
-    ordenDeCompra : new FormControl('', [Validators.required]),
-    
-    marca : new FormControl('', [Validators.required]),
-    descripcionContenido : new FormControl('', [Validators.required]),
-    pesoBruto : new FormControl('', [Validators.required]),
-    observaciones : new FormControl('', [Validators.required]),
-    montoTotalCompra : new FormControl('', [Validators.required]),
-    porcentajeGastosJustificados : new FormControl('', [Validators.required]),
-    sumaAseguradas : new FormControl('', [Validators.required]),
-    tasa : new FormControl('', [Validators.required]),
-    valorPrima : new FormControl('', [Validators.required]),
-    cobertura : new FormControl('', [Validators.required]),
-    deducible : new FormControl('', [Validators.required]),
-    objetoSeguro : new FormControl('', [Validators.required])
+    desde: new FormControl('', [Validators.required]), // texto
+    hasta: new FormControl('', [Validators.required]),
+    tipoTransporte: new FormControl('', [Validators.required]), //dropdown
+    perteneciente: new FormControl('', [Validators.required]),
+    fechaEmbarque: new FormControl(new Date(), [Validators.required]),
+    fechaLlegada: new FormControl(new Date(), [Validators.required]),
+    embarcadoPor: new FormControl('', [Validators.required]),
+    notaDePedido: new FormControl('', [Validators.required]), //dropdown
+    ordenDeCompra: new FormControl('', [Validators.required, Validators.min(0)]),
+
+    marca: new FormControl('', [Validators.required]),
+    descripcionContenido: new FormControl('', [Validators.required]),
+    pesoBruto: new FormControl('', [Validators.required]),
+    observaciones: new FormControl('', [Validators.required]),
+    montoTotalCompra: new FormControl('', [Validators.required]),
+    porcentajeGastosJustificados: new FormControl('', [Validators.required]),
+    sumaAseguradas: new FormControl('', [Validators.required]),
+    tasa: new FormControl('', [Validators.required]),
+    valorPrima: new FormControl('', [Validators.required]),
+    cobertura: new FormControl('', [Validators.required]),
+    deducible: new FormControl('', [Validators.required]),
+    objetoSeguro: new FormControl('', [Validators.required])
   });
 
   constructor(
     private router: Router,
     private applicationService: ApplicationService,
     @Inject(APP_CONFIG) public appConfig: AppConfig
-  ) { }
+  ) {
+    this.addApplicationForm.valueChanges.subscribe(console.log);
+
+  }
 
   onSubmit() {
     const request: AddApplication = {
@@ -56,7 +59,7 @@ export class AddApplicationComponent {
       embarcadoPor: this.addApplicationForm.get('embarcadoPor')?.value!,
       notaDePedido: this.addApplicationForm.get('notaDePedido')?.value!,
       ordenDeCompra: Number(this.addApplicationForm.get('ordenDeCompra')?.value!),
-      
+
       marca: this.addApplicationForm.get('marca')?.value!,
       descripcionContenido: this.addApplicationForm.get('descripcionContenido')?.value!,
       pesoBruto: Number(this.addApplicationForm.get('pesoBruto')?.value!),
